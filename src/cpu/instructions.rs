4ba2793117c859331997228e58bfdd1678ea5268
rravula <rishi.ravula@gmail.com>
2025-07-10 22:50:19 -0400

    Finished registers, impl instructions

---
+++ src/cpu/instructions.rs
@@ -0,0 +1,9 @@
+use super::registers::Registers;
+use super::registers::Flags;
+
+pub struct Instructions;
+
+impl Instructions {
+    //Todo: implement the instruction set, will later be inhereted by the CPU for opcode matching
+    // But we can put all the raw functionality here
+}
